a0455a43d96d1bf5aae1497615da2f9091c6d208
uncleguanghui <415558663@qq.com>
2020-11-12 18:46:29 +0800

    准备 kafka 的写入程序

---
+++ examples/4_window/kafka_monitor.py
@@ -0,0 +1,18 @@
+"""
+读取 kafka 的数据并打印
+"""
+from kafka import KafkaConsumer
+
+topic = 'user_click'
+bootstrap_servers = ['localhost:9092']
+group_id = 'group7'
+
+consumer = KafkaConsumer(
+    topic,  # topic的名称
+    group_id=group_id,  # 指定此消费者实例属于的组名，可以不指定
+    bootstrap_servers=bootstrap_servers,  # 指定kafka服务器
+    auto_offset_reset='latest',  # 'smallest': 'earliest', 'largest': 'latest'
+)
+
+for msg in consumer:
+    print(msg.value.decode('utf-8').encode('utf-8').decode('unicode_escape'))
---
+++ examples/4_window/kafka_producer.py
@@ -0,0 +1,84 @@
+import random
+import numpy as np
+from json import dumps
+from time import sleep
+from faker import Faker
+from datetime import datetime
+from kafka import KafkaProducer
+
+# ######################### 设置 #########################
+seed = 2020  # 设置随机数种子，保证每次运行的结果都一样
+num_users = 50  # 50 个用户
+max_msg_per_second = 20  # 每秒钟的最大消息数
+run_seconds = 3600  # 脚本最长运行时间，防止无限写入 kafka
+topic = "user_click"  # kafka topic
+bootstrap_servers = ['localhost:9092']
+
+fake = Faker(locale='zh_CN')
+Faker.seed(seed)
+random.seed(seed)
+
+
+class UserGroup:
+    def __init__(self):
+        """
+        为指定数量的用户分配不同的出现概率，每次按概率分布获取用户姓名
+        """
+        self.users = [self.gen_male() if random.random() < 0.6 else self.gen_female() for _ in range(num_users)]
+        prob = np.cumsum(np.random.uniform(1, 100, num_users))  # 用户点击概率的累加
+        self.prob = prob / prob.max()  # 压缩到 0 ~ 1
+
+    @staticmethod
+    def gen_male():
+        """
+        生成男人
+        """
+        return {'user_name': fake.name_male(), 'sex': '男'}
+
+    @staticmethod
+    def gen_female():
+        """
+        生成女人
+        """
+        return {'user_name': fake.name_female(), 'sex': '女'}
+
+    def get_user(self):
+        """
+        随机获得用户
+        """
+        r = random.random()  # 生成一个 0~1的随机数
+        index = np.searchsorted(self.prob, r)
+        return self.users[index]
+
+
+def write_data():
+    group = UserGroup()
+    start_time = datetime.now()
+
+    # 初始化生产者
+    producer = KafkaProducer(
+        bootstrap_servers=bootstrap_servers,
+        value_serializer=lambda x: dumps(x).encode('utf-8')
+    )
+
+    while True:
+        now = datetime.now()
+
+        # 生产数据，并发送到 kafka
+        user = group.get_user()
+        cur_data = {
+            "ts": now.strftime("%Y-%m-%d %H:%M:%S"),
+            "user_name": user['user_name'],
+            "sex": user['sex'],
+        }
+        producer.send(topic, value=cur_data)
+
+        # 终止条件
+        if (now - start_time).seconds > run_seconds:
+            break
+
+        # 停止时间
+        sleep(random.random() / max_msg_per_second)
+
+
+write_data()
